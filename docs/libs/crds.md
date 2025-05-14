eb1a7848aec4257362a31307601a3b4461aa8d66
René Schünemann <rene.schuenemann@sap.com>
2025-05-14 11:10:47 +0200

    add crd management library

--- docs/libs/crds.md
+++ docs/libs/crds.md
@@ -1,3 +1,62 @@
 # Custom Resource Definitions
 
-The `pkg/init/crds` package allows user to deploy CRDs from yaml files to a target cluster. It uses `embed.FS` to provide the files for deployment.
+The `pkg/crds` package allows user to deploy CRDs from yaml files to a target cluster.
+A typical use case is to use `embed.FS` to embed the CRDs in the controller binary and deploy them to the target clusters.
+
+The decision on which cluster a CRD should be deployed to is made by the `CRDManager` based on the labels of the CRDs and the labels of the clusters.
+The label key is passed to the `CRDManager` when it is created.
+Each cluster is then registered with a label value at the `CRDManager`.
+
+## Example
+
+```go
+package main
+
+import (
+	"context"
+	"embed"
+	
+	"github.com/openmcp-project/controller-utils/pkg/clusters"
+	"github.com/openmcp-project/controller-utils/pkg/crds"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+)
+
+//go:embed crds
+var crdsFS embed.FS
+var crdsPath = "crds"
+
+func main() {
+	ctx := context.Background()
+
+	onboardingCluster := clusters.NewTestClusterFromClient("onboarding", getOnboardingClient())
+	workloadCluster := clusters.NewTestClusterFromClient("workload", getWorkloadClient())
+
+	// use "openmcp.cloud/cluster" as the CRD label key
+	crdManager := crds.NewCRDManager("openmcp.cloud/cluster", func() ([]*apiextv1.CustomResourceDefinition, error) {
+		return crds.CRDsFromFileSystem(crdsFS, crdsPath)
+	})
+	
+	// register the onboarding cluster with label value "onboarding"
+	crdManager.AddCRDLabelToClusterMapping("onboarding", onboardingCluster)
+	// register the workload cluster with label value "workload"
+	crdManager.AddCRDLabelToClusterMapping("workload", workloadCluster)
+	
+	// create/update the CRDs in all clusters
+	err := crdManager.CreateOrUpdateCRDs(ctx, nil)
+	if err != nil {
+        panic(err)
+    }
+}
+```
+
+The CRDs need to be annotated with the label key and the label value of the cluster they should be deployed to.
+
+```yaml
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  name: testresources.example.com
+  labels:
+    openmcp.cloud/cluster: "onboarding"
+...
+```
---
+++ pkg/crds/crds_test.go
@@ -0,0 +1,86 @@
+package crds_test
+
+import (
+	"context"
+	"embed"
+	"testing"
+
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/openmcp-project/controller-utils/pkg/clusters"
+	utilstest "github.com/openmcp-project/controller-utils/pkg/testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/openmcp-project/controller-utils/pkg/crds"
+)
+
+//go:embed testdata/*
+var testFS embed.FS
+
+func TestCRDs(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "CRDs Test Suite")
+}
+
+var _ = Describe("CRDsFromFileSystem", func() {
+	It("should correctly read and parse CRDs from the filesystem", func() {
+		crdPath := "testdata"
+		crdsList, err := crds.CRDsFromFileSystem(testFS, crdPath)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(crdsList).To(HaveLen(2))
+
+		// Validate the first CRD
+		Expect(crdsList[0].Name).To(Equal("testresources.example.com"))
+		Expect(crdsList[0].Spec.Names.Kind).To(Equal("TestResource"))
+
+		// Validate the second CRD
+		Expect(crdsList[1].Name).To(Equal("sampleresources.example.com"))
+		Expect(crdsList[1].Spec.Names.Kind).To(Equal("SampleResource"))
+	})
+})
+
+var _ = Describe("CRDManager", func() {
+	It("should correctly manage CRD mappings and create/update CRDs", func() {
+		scheme := runtime.NewScheme()
+		err := apiextv1.AddToScheme(scheme)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Create fake clients
+		clientA, err := utilstest.GetFakeClient(scheme)
+		Expect(err).NotTo(HaveOccurred())
+
+		clientB, err := utilstest.GetFakeClient(scheme)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Create fake clusters
+		clusterA := clusters.NewTestClusterFromClient("cluster_a", clientA)
+		clusterB := clusters.NewTestClusterFromClient("cluster_b", clientB)
+
+		crdManager := crds.NewCRDManager("openmcp.cloud/cluster", func() ([]*apiextv1.CustomResourceDefinition, error) {
+			return crds.CRDsFromFileSystem(testFS, "testdata")
+		})
+
+		crdManager.AddCRDLabelToClusterMapping("cluster_a", clusterA)
+		crdManager.AddCRDLabelToClusterMapping("cluster_b", clusterB)
+
+		ctx := context.Background()
+
+		err = crdManager.CreateOrUpdateCRDs(ctx, nil)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Verify that the CRDs were created in the respective clusters
+		crdA := &apiextv1.CustomResourceDefinition{}
+		err = clientA.Get(ctx, types.NamespacedName{Name: "testresources.example.com"}, crdA)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(crdA.Labels["openmcp.cloud/cluster"]).To(Equal("cluster_a"))
+
+		crdB := &apiextv1.CustomResourceDefinition{}
+		err = clientB.Get(ctx, types.NamespacedName{Name: "sampleresources.example.com"}, crdB)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(crdB.Labels["openmcp.cloud/cluster"]).To(Equal("cluster_b"))
+	})
+})
