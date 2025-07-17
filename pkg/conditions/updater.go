e5e349ec6d9cd09ce09859a7373783ea6138aec4
Johannes Aubart <johannes.aubart@sap.com>
2025-07-17 11:38:09 +0200

    task generate

--- pkg/conditions/updater.go
+++ pkg/conditions/updater.go
@@ -5,12 +5,13 @@ import (
 	"slices"
 	"strings"
 
-	"github.com/openmcp-project/controller-utils/pkg/collections"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/record"
+
+	"github.com/openmcp-project/controller-utils/pkg/collections"
 )
 
 const EventReasonConditionChanged = "ConditionChanged"
